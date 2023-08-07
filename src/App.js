5846a67837161a3d491c21b9a383f151c2809b58
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2023-08-07 09:58:23 +0300

    github

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import AnimatedRoutes from "./components/AnimatedRoutes";
 
 function App() {
   const personalDetails = {
-    name: "Michael Yeates",
-    location: "Loughborough, UK",
-    email: "michael-yeates@outlook.com",
+    name: "Hadeer Hassan",
+    location: "Cairo, Egypt",
+    email: "eng.hadeer2893@gmail.com",
     availability: "Open for work",
-    brand:
-      "My unique blend of technical expertise, creative thinking, and background in psychology allows me to approach each project with a deep understanding of the end user's perspective, resulting in highly effective user-centred digital products.",
+    // brand:
+    //   "My unique blend of technical expertise, creative thinking, and background in psychology allows me to approach each project with a deep understanding of the end user's perspective, resulting in highly effective user-centred digital products.",
+    brand: "Software Engineer | ITI Graduated | AAST ",
   };
 
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import NavLinks from "./NavLinks";
-import logo from "../images/logo.svg";
+import logo from "../img/logo.png";
 
 const Header = () => {
   return (
     <header className="header">
-      <img className="logo" src={logo} alt="Michael Yeates' Logo" />
+      <img
+        className="logo"
+        src={logo}
+        alt="Hadeer's Logo"
+        width={200}
+        // height={100}
+      />
       <NavLinks />
     </header>
   );
