008b54d5e9aef2b2432e4d8779b5472fdf3cedd1
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2023-09-11 11:51:21 +0300

    try transperant nav

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -8,14 +8,17 @@ const NavLinks = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <>
-      <button className="dropdown-toggle" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+      <button
+        className="dropdown-toggle"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
         {isMenuOpen ? (
           <img className="closeMenu" src={closeMenu} alt="Close" />
         ) : (
           <img className="openMenu" src={openMenu} alt="Open" />
         )}
       </button>
-      <nav className={`links ${isMenuOpen ? "open" : "closed"}`}>
+      <nav className={`links ${isMenuOpen ? "open" : "closed"} `}>
         <NavLink to="/" onClick={() => setIsMenuOpen(false)}>
           Home
         </NavLink>
