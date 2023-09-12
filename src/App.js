8de166ad7cf0251208c6b5d52dc203428ec86ee5
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2023-09-12 15:22:52 +0300

    final

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Header from "./components/Header";
+import AnimatedRoutes from "./components/AnimatedRoutes";
+
+function App() {
+  const personalDetails = {
+    name: "Hadeer Hassan",
+    location: "Cairo, Egypt",
+    email: "eng.hadeer2893@gmail.com",
+    availability: "Open for work",
+    // brand:
+    //   "My unique blend of technical expertise, creative thinking, and background in psychology allows me to approach each project with a deep understanding of the end user's perspective, resulting in highly effective user-centred digital products.",
+    brand: "Front-End Developer | ITI Graduated | AAST ",
+  };
+
+  return (
+    <>
+      <Header />
+      <AnimatedRoutes personalDetails={personalDetails} />
+    </>
+  );
+}
+
+export default App;
